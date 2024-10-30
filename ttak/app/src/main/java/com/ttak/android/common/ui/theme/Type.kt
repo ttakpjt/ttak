61dd5b51e5ee727d06472ff3fd810d346dd6543c
이규석 <didie8520@gmail.com>
2024-10-30 15:11:57 +0900

    design: 배경 검정, 글자색 흰, 폰트 Pretendard
    - Color.kt에 사용하는 색상 8가지 정의
    - Type.kt에 사용하는 폰트 3가지 정의
    - Theme.kt에 배경, 글자색, 폰트 적용

--- ttak/app/src/main/java/com/ttak/android/common/ui/theme/Type.kt
+++ ttak/app/src/main/java/com/ttak/android/common/ui/theme/Type.kt
@@ -1,34 +1,40 @@
-package com.ttak.android.ui.theme
+package com.ttak.android.common.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.ttak.android.R
 
-// Set of Material typography styles to start with
+val Pretendard = FontFamily(
+    Font(R.font.pretendard_regular, FontWeight.Normal),
+    Font(R.font.pretendard_semi_bold, FontWeight.SemiBold),
+    Font(R.font.pretendard_bold, FontWeight.Bold)
+)
+
+// 본문, 제목, 소제목에 사용할 폰트 정의
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Pretendard,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Bold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
