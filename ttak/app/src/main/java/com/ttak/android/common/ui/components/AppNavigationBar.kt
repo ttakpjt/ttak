9019baee370a83d235670f27c099b3c395d29265
이규석 <didie8520@gmail.com>
2024-10-31 19:18:15 +0900

    chore: AppNavigationBar 코드 정리
    - 불필요한 import 수정

--- ttak/app/src/main/java/com/ttak/android/common/ui/components/AppNavigationBar.kt
+++ ttak/app/src/main/java/com/ttak/android/common/ui/components/AppNavigationBar.kt
@@ -5,14 +5,12 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -76,7 +74,8 @@ fun BottomNavItem(
                     popUpTo(AppScreens.Home.route) { inclusive = false }
                     launchSingleTop = true
                 }
-            },
+            }
+            .pointerInput(Unit) {},  // 호버 이벤트 무시
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Icon(
