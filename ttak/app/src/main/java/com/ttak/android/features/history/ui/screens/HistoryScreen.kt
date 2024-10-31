9428fbe1997c994620474cbec5e68154a3beb67d
이규석 <didie8520@gmail.com>
2024-10-31 15:07:29 +0900

    chore: 각 기능 별 화면 추가
    - 네비게이션 이동 테스트를 위한 Screen 추가

--- ttak/app/src/main/java/com/ttak/android/features/history/ui/screens/HistoryScreen.kt
+++ ttak/app/src/main/java/com/ttak/android/features/history/ui/screens/HistoryScreen.kt
@@ -1,1 +1,8 @@
 package com.ttak.android.features.history.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun HistoryScreen(navController: NavController) {
+}
