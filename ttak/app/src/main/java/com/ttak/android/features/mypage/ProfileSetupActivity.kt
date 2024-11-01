75d6c696317413b587b7d95ceecbff25d025fcb7
이규석 <didie8520@gmail.com>
2024-11-01 20:55:43 +0900

    design: 프로필 설정 화면 추가
    - ProfileSetupActivity 생성
    - ProfileSetupScreen 생성

--- ttak/app/src/main/java/com/ttak/android/features/mypage/ProfileSetupActivity.kt
+++ ttak/app/src/main/java/com/ttak/android/features/mypage/ProfileSetupActivity.kt
@@ -18,6 +18,7 @@ class ProfileSetupActivity : ComponentActivity() {
         setContent {
             TtakTheme {
                 // Compose 내에서 상태 정의
+                val nickname = remember { mutableStateOf("") }
                 val profileImageUri = remember { mutableStateOf<Uri?>(null) }
 
                 // 이미지 선택 런처 정의
