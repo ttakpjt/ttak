1ee8a3f3dfc5f63ed6f5f78519083ff0843c3b81
fasongsong <148851703+songsongyejin@users.noreply.github.com>
2024-11-01 14:10:23 +0900

    #5 [chore] : websocket 설정

---
+++ backend/src/main/java/com/ttak/backend/global/config/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package com.ttak.backend.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		//클라이언트가 구독한 url
+		config.enableSimpleBroker("/topic");
+
+		// 클라이언트가 메시지를 보낼 때 사용할 경로 접두사 설정
+		config.setApplicationDestinationPrefixes("/app/status");
+	}
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		// 웹소켓 연결 엔드포인트
+		registry.addEndpoint("/ws")
+			.setAllowedOriginPatterns("*") //CORS 설정
+			.withSockJS();
+	}
+}
