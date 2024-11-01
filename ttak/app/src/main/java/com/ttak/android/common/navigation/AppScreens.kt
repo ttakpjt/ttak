9a1c32ec23e6ef0d89cd37139e447a268765a288
이규석 <didie8520@gmail.com>
2024-11-01 20:48:30 +0900

    style: 스플래시, 로그인 코드 정리
    - AppNavHost에서 불필요한 import 삭제
    - AppScreens에서 불필요한 정의 삭제
    - AndroidManifest.xml를 수정하여 SplashActivity부터 수행되도록 조정

--- ttak/app/src/main/java/com/ttak/android/common/navigation/AppScreens.kt
+++ ttak/app/src/main/java/com/ttak/android/common/navigation/AppScreens.kt
@@ -1,8 +1,6 @@
 package com.ttak.android.common.navigation
 
 sealed class AppScreens(val route: String) {
-    object Home : AppScreens("home")
-    object Login : AppScreens("login")
     object History : AppScreens("history")
     object MyPage : AppScreens("my_page")
     object Observer : AppScreens("observer")
