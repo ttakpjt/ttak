32592664e97ecd3cb89d99172ba38aa3dcf71a6a
이규석 <didie8520@gmail.com>
2024-11-01 20:57:54 +0900

    fix: AppNavigationBar.kt에서 home -> screentime으로 변경
    - LoginActivity에 주석 추가
    - 불필요한 import 구문 삭제

--- ttak/app/src/main/java/com/ttak/android/features/mypage/ui/screens/ProfileSetupScreen.kt
+++ ttak/app/src/main/java/com/ttak/android/features/mypage/ui/screens/ProfileSetupScreen.kt
@@ -1,7 +1,6 @@
 package com.ttak.android.features.mypage.ui.screens
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.activity.result.ActivityResultLauncher
@@ -15,7 +14,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
