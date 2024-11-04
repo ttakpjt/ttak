f2b056b0e9e8bc236c288775a147a8b1e12ec292
이규석 <didie8520@gmail.com>
2024-11-04 11:13:06 +0900

    feat: 프로필 설정 기능 완성
    - 프로필 사진 선택 요소 컴포넌트 ProfileImagePicker로 분리
    - ProfileSetupScreen의 각 요소 조정
    - ProfileSetupScreen에서 이미지가 선택되지 않았을 때 회색 배경이 보이게 변경

--- ttak/app/src/main/java/com/ttak/android/features/mypage/ui/screens/ProfileSetupScreen.kt
+++ ttak/app/src/main/java/com/ttak/android/features/mypage/ui/screens/ProfileSetupScreen.kt
@@ -1,25 +1,18 @@
 package com.ttak.android.features.mypage.ui.screens
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.activity.result.ActivityResultLauncher
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -27,81 +20,60 @@ import com.ttak.android.MainActivity
 import com.ttak.android.R
 import com.ttak.android.common.ui.components.AppButton
 import com.ttak.android.common.ui.components.AppSearchBar
-import com.ttak.android.common.ui.theme.Grey
-import coil.compose.rememberAsyncImagePainter
-
+import com.ttak.android.common.ui.theme.Blue
+import com.ttak.android.common.ui.theme.Black
+import com.ttak.android.common.ui.theme.White
+import com.ttak.android.features.mypage.ui.components.ProfileImagePicker
 
 @Composable
 fun ProfileSetupScreen(
     profileImageUri: MutableState<Uri?>,
     selectImageLauncher: ActivityResultLauncher<String>
 ) {
-    val context = LocalContext.current // Context 가져오기
+    val context = LocalContext.current
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
+            .background(Black)
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        // 상단 프로필 소제목
         Text(
             text = "프로필 설정",
-            fontSize = 20.sp,
+            fontSize = 32.sp,
             fontWeight = FontWeight.Bold,
-            color = MaterialTheme.colorScheme.onBackground,
+            color = White,
             modifier = Modifier.align(Alignment.Start)
         )
 
         Spacer(modifier = Modifier.height(32.dp))
 
-        // 프로필 이미지를 선택하는 부분
-        Box(
-            contentAlignment = Alignment.BottomEnd,
-            modifier = Modifier.size(120.dp)
-        ) {
-            Image(
-                painter = if (profileImageUri.value != null) {
-                    rememberAsyncImagePainter(profileImageUri.value) // URI를 통한 이미지 로딩
-                } else {
-                    painterResource(R.drawable.default_proflie)
-                },
-                contentDescription = "프로필 이미지",
-                modifier = Modifier
-                    .size(100.dp)
-                    .clip(CircleShape)
-                    .background(Grey)
-            )
-            IconButton(
-                onClick = { selectImageLauncher.launch("image/*") },
-                modifier = Modifier
-                    .size(32.dp)
-                    .clip(CircleShape)
-                    .background(MaterialTheme.colorScheme.primary)
-            ) {
-                Icon(
-                    painter = painterResource(R.drawable.image),
-                    contentDescription = "프로필 이미지 선택",
-                    tint = Color.White
-                )
-            }
-        }
+        // 프로필 사진 설정 화면
+        ProfileImagePicker(
+            profileImageUri = profileImageUri,
+            selectImageLauncher = selectImageLauncher
+        )
 
         Spacer(modifier = Modifier.height(24.dp))
 
-        // 닉네임을 입력하고 중복 검사하는 부분
+        // 닉네임 입력
         AppSearchBar(
             icon = R.drawable.ic_check_circle,
             modifier = Modifier.fillMaxWidth()
         )
 
-        Spacer(modifier = Modifier.height(24.dp))
+        Spacer(modifier = Modifier.height(48.dp))
 
-        // 이미지를 db에 저장하는 로직도 추가해야 함
+        // 저장하고 다음 화면으로 넘어가는 버튼
         AppButton(
             text = "저장",
+            backgroundColor = Blue,
+            contentColor = Black,
             onClick = {
+                val sharedPreferences = context.getSharedPreferences("AppPrefs", Context.MODE_PRIVATE)
+                sharedPreferences.edit().putBoolean("isProfileSetupComplete", true).apply()
+
                 context.startActivity(Intent(context, MainActivity::class.java))
                 (context as? Activity)?.finish()
             }
