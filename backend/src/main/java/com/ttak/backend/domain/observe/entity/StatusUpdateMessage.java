cb6faad874ac07cf2b63023f9695d0e2eb0f89de
fasongsong <148851703+songsongyejin@users.noreply.github.com>
2024-11-06 13:52:28 +0900

    #31 [refactor] : status 주석 수정

--- backend/src/main/java/com/ttak/backend/domain/observe/entity/StatusUpdateMessage.java
+++ backend/src/main/java/com/ttak/backend/domain/observe/entity/StatusUpdateMessage.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public class StatusUpdateMessage {
 	private Long userId;      // 금지된 행동을 한 사용자 ID
-	private int status;   // 금지된 행동의 유형 1: Green, -1: red
+	private int status;   // 금지된 행동의 유형 0:Green, 1:red
 
 	private StatusUpdateMessage(Long userId, int status) {
 		this.userId = userId;
