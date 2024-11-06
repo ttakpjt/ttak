69e9783dbc2f3f3119043eedaf6337240f7b96a8
fasongsong <148851703+songsongyejin@users.noreply.github.com>
2024-11-06 13:09:36 +0900

    #31 [refactor] : userState int타입으로 변경

--- backend/src/main/java/com/ttak/backend/domain/observe/entity/StatusUpdateMessage.java
+++ backend/src/main/java/com/ttak/backend/domain/observe/entity/StatusUpdateMessage.java
@@ -1,15 +1,13 @@
 package com.ttak.backend.domain.observe.entity;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 
 @Getter
 public class StatusUpdateMessage {
 	private Long userId;      // 금지된 행동을 한 사용자 ID
-	private UserStatus status;   // 금지된 행동의 유형
+	private int status;   // 금지된 행동의 유형 1: Green, -1: red
 
-	private StatusUpdateMessage(Long userId, UserStatus status) {
+	private StatusUpdateMessage(Long userId, int status) {
 		this.userId = userId;
 		this.status = status;
 	}
