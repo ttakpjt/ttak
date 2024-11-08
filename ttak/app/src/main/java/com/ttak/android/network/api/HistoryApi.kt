7dbd479da8776967a99c1b974a620281349248cd
chris2769 <chris2769@naver.com>
2024-11-08 16:56:02 +0900

    #41 [feat] : 주간 딱걸림 횟수 가져오는 API 추가

--- ttak/app/src/main/java/com/ttak/android/network/api/HistoryApi.kt
+++ ttak/app/src/main/java/com/ttak/android/network/api/HistoryApi.kt
@@ -1,12 +1,16 @@
 package com.ttak.android.network.api
 
 import com.ttak.android.domain.model.HistoryInfo
+import com.ttak.android.domain.model.WeeklyPickResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.Response
 
 interface HistoryApi {
+
+    @GET("history/pick")
+    suspend fun getWeeklyPickCount(): Response<WeeklyPickResponse>
     
     @GET("messages")
     suspend fun getMessages(): List<HistoryInfo>
