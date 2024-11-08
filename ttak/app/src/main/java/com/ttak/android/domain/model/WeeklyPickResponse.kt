ecb1131322e01ee9d09a2409bcda0771325bb771
chris2769 <chris2769@naver.com>
2024-11-08 16:59:07 +0900

    #41 [feat] : 주간 딱걸림 횟수 response 데이터 받기 위한 data 객체 구현

---
+++ ttak/app/src/main/java/com/ttak/android/domain/model/WeeklyPickResponse.kt
@@ -0,0 +1,11 @@
+package com.ttak.android.domain.model
+
+data class WeeklyPickResponse(
+    val code: String,
+    val message: String,
+    val data: WeeklyPickData
+)
+
+data class WeeklyPickData(
+    val myCount: Int
+)
