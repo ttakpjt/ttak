43ac435fb0d19edf62e036a1285e7260deed6ef0
fasongsong <148851703+songsongyejin@users.noreply.github.com>
2024-11-08 17:14:07 +0900

    #60 [feat] : 사용자 검색에 친구 관계 정보 추가

--- backend/src/main/java/com/ttak/backend/domain/user/repository/customRepository/CustomUserRepositoryImpl.java
+++ backend/src/main/java/com/ttak/backend/domain/user/repository/customRepository/CustomUserRepositoryImpl.java
@@ -1,6 +1,13 @@
 package com.ttak.backend.domain.user.repository.customRepository;
 
+import java.util.List;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ttak.backend.domain.observe.entity.QFriend;
+import com.ttak.backend.domain.user.dto.response.UserInfoResponse;
+import com.ttak.backend.domain.user.entity.QUser;
 
 import lombok.RequiredArgsConstructor;
 
@@ -9,5 +16,26 @@ public class CustomUserRepositoryImpl implements CustomUserRepository {
 
 	private final JPAQueryFactory jpaQueryFactory;
 
+	public List<UserInfoResponse> findUsersWithRelation(Long currentUserId, String nickname) {
+		QUser user = QUser.user;
+		QFriend friend = QFriend.friend;
+
+		return jpaQueryFactory
+			.select(Projections.constructor(
+				UserInfoResponse.class,
+				user.nickname,
+				user.userId,
+				user.profilePic,
+				new CaseBuilder()
+					.when(user.userId.eq(currentUserId)).then("Self")
+					.when(friend.userId.userId.eq(currentUserId).and(friend.followingId.userId.eq(user.userId)))
+					.then("Friend")
+					.otherwise("None")
+			))
+			.from(user)
+			.leftJoin(friend).on(friend.followingId.userId.eq(user.userId).and(friend.userId.userId.eq(currentUserId)))
+			.where(user.nickname.containsIgnoreCase(nickname))
+			.fetch();
+	}
 
 }
