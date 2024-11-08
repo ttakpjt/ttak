f105d5aee67535545c6f700909eaf4f6c2065498
chris2769 <chris2769@naver.com>
2024-11-08 17:01:04 +0900

    #41 [feat] : 딱걸림 횟수 받아와서 화면에 보여주는 기능 및 화면 구성 구현

--- ttak/app/src/main/java/com/ttak/android/features/history/ui/screens/HistoryScreen.kt
+++ ttak/app/src/main/java/com/ttak/android/features/history/ui/screens/HistoryScreen.kt
@@ -1,25 +1,36 @@
 package com.ttak.android.features.history.ui.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.ttak.android.data.repository.HistoryRepositoryImpl
 import com.ttak.android.features.history.ui.components.HistoryMessageItem
 import com.ttak.android.features.history.ui.components.SystemNotificationCard
 import com.ttak.android.features.history.viewmodel.HistoryViewModel
 import com.ttak.android.features.history.viewmodel.HistoryViewModelFactory
-import com.ttak.android.network.api.PreviewHistoryApi
+import com.ttak.android.network.api.NetworkModule
 
 @Composable
 fun HistoryScreen(
     viewModel: HistoryViewModel = viewModel(
-        factory = HistoryViewModelFactory(PreviewHistoryApi())
+        factory = HistoryViewModelFactory(
+            repository = HistoryRepositoryImpl(
+                api = NetworkModule.provideHistoryApi()
+            )
+        )
     )
 ) {
+
+    val weeklyPickCount by viewModel.weeklyPickCount.collectAsState()
     val messages by viewModel.messages.collectAsState()
     val systemNotification by viewModel.systemNotification.collectAsState()
 
@@ -29,7 +40,7 @@ fun HistoryScreen(
             .background(Color(0xFF0E181E))
     ) {
         SystemNotificationCard(
-            notificationText = "주간 딱걸림\n10번",
+            notificationText = "주간 딱걸림\n${weeklyPickCount}번",
             watchingCount = 5
         )
 
