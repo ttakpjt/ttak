8c1f4f0cc4e2056fb1033218ac9402fc5a84407a
fasongsong <148851703+songsongyejin@users.noreply.github.com>
2024-11-12 15:08:16 +0900

    #82 [refactor] : 필요없는 메소드 주석

--- backend/src/main/java/com/ttak/backend/domain/observe/repository/customRepository/CustomFriendRepository.java
+++ backend/src/main/java/com/ttak/backend/domain/observe/repository/customRepository/CustomFriendRepository.java
@@ -8,7 +8,7 @@ import com.ttak.backend.domain.observe.dto.FriendInfoResponse;
 import com.ttak.backend.domain.user.entity.User;
 
 public interface CustomFriendRepository {
-	List<FriendInfoResponse> findBannedFriendsByLocalTime(User user, LocalTime currentTime);
+	// List<FriendInfoResponse> findBannedFriendsByLocalTime(User user, LocalTime currentTime);
 	List<FriendInfoResponse> findBannedFriendsByLocalDateTime(User user, LocalDateTime currentTime);
 	long countFollowers(Long userId);
 }
