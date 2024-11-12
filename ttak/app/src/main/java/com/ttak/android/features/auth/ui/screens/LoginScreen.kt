07a80027255e4e4827bc5099a4380c6be824268e
이규석 <didie8520@gmail.com>
2024-11-12 14:35:22 +0900

    #66 [design] : 메시지를 보낼 때, 텍스트 색상 검정색으로 변경
    - 불필요한 import 구문 수정

--- ttak/app/src/main/java/com/ttak/android/features/auth/ui/screens/LoginScreen.kt
+++ ttak/app/src/main/java/com/ttak/android/features/auth/ui/screens/LoginScreen.kt
@@ -1,7 +1,6 @@
 package com.ttak.android.features.auth.ui.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -14,9 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.ttak.android.R
 import com.ttak.android.common.ui.theme.White
 
@@ -68,8 +65,6 @@ fun LoginScreen(onLoginClick: () -> Unit) {
                         "Google로 시작하기",
                         color = Color.Black,
                         style = MaterialTheme.typography.bodySmall
-//                        fontWeight = FontWeight.Bold,
-//                        fontSize = 16.sp
                     )
                 }
             }
