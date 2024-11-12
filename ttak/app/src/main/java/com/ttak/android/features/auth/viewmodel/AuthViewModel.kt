21d114c1bf7938e829a20af3b1614e9053977164
이규석 <didie8520@gmail.com>
2024-11-12 11:04:54 +0900

    #66 [feat] : 기존 계정이 존재할 때, 기존 닉네임을 사용하도록 변경
    - user/exist/nickname API 추가
    - retrofit2의 Response 방식 활용

--- ttak/app/src/main/java/com/ttak/android/features/auth/viewmodel/AuthViewModel.kt
+++ ttak/app/src/main/java/com/ttak/android/features/auth/viewmodel/AuthViewModel.kt
@@ -3,7 +3,7 @@ package com.ttak.android.features.auth.viewmodel
 import android.app.Application
 import androidx.lifecycle.viewModelScope
 import com.ttak.android.data.repository.MemberRepository
-import com.ttak.android.domain.model.UserModel
+import com.ttak.android.domain.model.MemberRequest
 import kotlinx.coroutines.launch
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
@@ -16,7 +16,7 @@ class MemberViewModel(application: Application) : AndroidViewModel(application) 
         MemberApiImpl(ApiConfig.createMemberApi(application))
 
     // signIn 메소드: 로그인 처리 후 test 호출
-    fun signIn(user: UserModel, onResult: (Boolean) -> Unit) {
+    fun signIn(user: MemberRequest, onResult: (Boolean) -> Unit) {
         // 비동기 요청 수행
         viewModelScope.launch {
             try {
@@ -35,8 +35,35 @@ class MemberViewModel(application: Application) : AndroidViewModel(application) 
                     }
                 }
             } catch (e: Exception) {
-                Log.e("이규석", "로그인 실패: $e")
+                Log.e("귯", "로그인 실패: $e")
                 onResult(false)
+            }
+        }
+    }
+
+    // 로그아웃
+    fun logout() {
+        viewModelScope.launch {
+            val result = memberRepository.logout()
+            result.let { result ->
+                if (result.isSuccess) {
+                    // 로그아웃 성공
+                    Log.d("귯", "로그아웃 성공")
+                } else {
+                    // 로그아웃 실패 처리
+                    Log.e("귯", "로그아웃 실패: ${result.exceptionOrNull()}")
+                }
+            }
+        }
+    }
+
+    // 서버에 닉네임이 존재하는지 확인
+    fun existNickname(onNicknameChecked: (String) -> Unit) {
+        viewModelScope.launch {
+            val response = memberRepository.existNickname().body()
+            // 닉네임 전달
+            response?.let {
+                onNicknameChecked(it.data)
             }
         }
     }
--- ttak/app/src/main/java/com/ttak/android/network/api/MemberApi.kt
+++ ttak/app/src/main/java/com/ttak/android/network/api/MemberApi.kt
@@ -1,28 +1,25 @@
 package com.ttak.android.network.api
 
-import com.ttak.android.domain.model.UserModel
+import com.ttak.android.domain.model.MemberResponse
+
+import com.ttak.android.domain.model.SignInResponse
+import com.ttak.android.domain.model.MemberRequest
 import retrofit2.Response
 import retrofit2.http.POST
 import retrofit2.http.GET
 import retrofit2.http.Body
 
 
-data class SignInResponse(
-    val code: String,
-    val message: String,
-    val data: UserData,
-)
-
-data class UserData(
-    val userId: String
-)
-
-data class StringData(
-    val data: String
-)
-
 interface MemberApi {
     // 구글 계정 정보 보내기
     @POST("user/save")
-    suspend fun signIn(@Body user: UserModel): Response<SignInResponse>
+    suspend fun signIn(@Body user: MemberRequest): Response<SignInResponse>
+
+    // 로그아웃
+    @GET("logout")
+    suspend fun logout(): Response<MemberResponse>
+
+    // 닉네임이 존재하는지 확인
+    @GET("user/exist/nickname")
+    suspend fun existNickname(): Response<MemberResponse>
 }
