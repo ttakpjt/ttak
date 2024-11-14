c5650a775a9dc100e85bbaca9e88bb8515a243f1
JungHyeon Hwang <wjdgus982@naver.com>
2024-11-14 10:01:32 +0900

    #94 [fix] : fcm Message 객체 설정 추가 (Android)

--- backend/src/main/java/com/ttak/backend/domain/user/controller/BugController.java
+++ backend/src/main/java/com/ttak/backend/domain/user/controller/BugController.java
@@ -2,7 +2,6 @@ package com.ttak.backend.domain.user.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/bug")
 @Tag(name = "bug API", description = "버그 제보 및 설정 관련 api")
 @CrossOrigin("*")
-public class BanController {
+public class BugController {
 
 	private final BugService bugService;
 
