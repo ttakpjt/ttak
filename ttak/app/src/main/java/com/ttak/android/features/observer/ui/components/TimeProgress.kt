bf9b69acf5e97437f7749f21e99b93b87ab27fde
chris2769 <chris2769@naver.com>
2024-11-18 01:18:07 +0900

    #101 [feat] : 시간 설정 바 UI 변경

--- ttak/app/src/main/java/com/ttak/android/features/observer/ui/components/TimeProgress.kt
+++ ttak/app/src/main/java/com/ttak/android/features/observer/ui/components/TimeProgress.kt
@@ -21,7 +21,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ttak.android.R
@@ -78,6 +77,7 @@ fun TimeProgress(
                 text = formatTimeRange(startTime, endTime),
                 fontSize = 18.sp,
                 style = MaterialTheme.typography.titleMedium,
+                color = Color.White
             )
         }
 
