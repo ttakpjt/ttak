1e456afc6f898de2e17e66d51d547c4ff7cbc93f
chris2769 <chris2769@naver.com>
2024-11-17 15:59:44 +0900

    #90 [feat] : ignore 추가 및 물풍선 버튼 제거

--- ttak/app/src/main/java/com/ttak/android/features/observer/ui/components/ExpandableFriendListContainer.kt
+++ ttak/app/src/main/java/com/ttak/android/features/observer/ui/components/ExpandableFriendListContainer.kt
@@ -4,14 +4,26 @@ import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.pointer.PointerInputChange
@@ -20,7 +32,6 @@ import androidx.compose.ui.unit.dp
 import com.ttak.android.domain.model.FilterOption
 import com.ttak.android.domain.model.FriendStory
 import com.ttak.android.domain.model.User
-import kotlinx.coroutines.async
 import kotlin.math.abs
 
 @Composable
