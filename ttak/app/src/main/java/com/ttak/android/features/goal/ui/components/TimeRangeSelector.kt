d963c986ed2abd3b6e56b198ed9b5e2b45e3f3cd
chris2769 <chris2769@naver.com>
2024-11-18 19:41:09 +0900

    #106 [feat] : 시간 설정 UI 변경

--- ttak/app/src/main/java/com/ttak/android/features/goal/ui/components/TimeRangeSelector.kt
+++ ttak/app/src/main/java/com/ttak/android/features/goal/ui/components/TimeRangeSelector.kt
@@ -1,5 +1,6 @@
 package com.ttak.android.features.goal.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
